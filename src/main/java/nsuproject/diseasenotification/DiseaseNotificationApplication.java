6436294a388a762f6d6441b1111e3b0d9e342882
YH3041 <mmj2rv9@gmail.com>
2021-03-19 17:53:52 +0900

    Initial commit

---
+++ src/main/java/nsuproject/diseasenotification/DiseaseNotificationApplication.java
@@ -0,0 +1,13 @@
+package nsuproject.diseasenotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiseaseNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiseaseNotificationApplication.class, args);
+	}
+
+}
